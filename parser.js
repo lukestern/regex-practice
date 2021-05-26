2763ca7681fb5c9a7842402fd9a3a6866f3f8758
Luke Stern <luke.stern@softwire.com>
2021-05-26 11:06:10 +0100

    Exercise part 2

--- parser.js
+++ parser.js
@@ -4,8 +4,9 @@ const fl = fs.readFileSync('test.txt', 'utf8');
 const flArray = fl.split(' ')
 
 let numberOfSoftwireEmails = 0;
+let search = new RegExp('.@softwire\.com');
 for (let i = 0; i < flArray.length; i++) {
-    if (flArray[i].includes('@softwire.com') ) {
+    if (flArray[i].match(search)) {
         numberOfSoftwireEmails++
     }
 }
